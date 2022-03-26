69bd7da5796754c16baa4ef9cc89340abeb4e7d0
BrainsOfSteel <milindsah1234@gmail.com>
2022-03-26 20:18:00 +0530

    docker-compose for FE and api

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -16,7 +16,7 @@ const Search = ({word, setWord, handleSubmit}) => {
                 />
                 </Col>
                 <Col>
-                  <Button variant="primary" type="submit"> Search </Button>
+                  <Button variant="primary" type="submit"> Search</Button>
                 </Col>
               </Form.Row>
             </Form>
